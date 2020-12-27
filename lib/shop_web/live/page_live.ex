ca78b8d73db8f3c47f86287fcbd95bdc4041fd4e
adam-james <adam.j.sulewski@gmail.com>
2020-12-27 14:19:37 -0800

    pubsub cart changes

--- lib/shop_web/live/page_live.ex
+++ lib/shop_web/live/page_live.ex
@@ -7,18 +7,28 @@ defmodule ShopWeb.PageLive do
   @impl true
   def mount(_params, %{"cart" => cart}, socket) do
     products = Catalog.list_products()
+    ShopWeb.Endpoint.subscribe("cart:#{cart.id}")
     {:ok, assign(socket, products: products, cart: cart)}
   end
 
-  def handle_event("add-to-cart", %{"id" => id} = value, socket) do
+  @impl true
+  def handle_event("add-to-cart", %{"id" => id} = _value, socket) do
     product = Catalog.get_product!(id)
     {:ok, cart} = Shopper.add_to_cart(socket.assigns.cart, product)
+    broadcast(cart)
     {:noreply, assign(socket, cart: cart)}
   end
 
-  def handle_event("remove-from-cart", %{"id" => id} = value, socket) do
+  @impl true
+  def handle_event("remove-from-cart", %{"id" => id} = _value, socket) do
     cart_item = Shop.Repo.get(Shopper.CartItem, id)
     {:ok, cart} = Shopper.remove_from_cart(cart_item)
+    broadcast(cart)
+    {:noreply, assign(socket, cart: cart)}
+  end
+
+  @impl true
+  def handle_info(%{event: "updated_cart", payload: cart}, socket) do
     {:noreply, assign(socket, cart: cart)}
   end
 
@@ -27,5 +37,9 @@ defmodule ShopWeb.PageLive do
     cents = rem(price, 100)
 
     "$" <> to_string(dollars) <> "." <> to_string(cents)
+  end
+
+  defp broadcast(cart) do
+    ShopWeb.Endpoint.broadcast!("cart:#{cart.id}", "updated_cart", cart)
   end
 end
