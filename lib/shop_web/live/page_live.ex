cd986fa327d8920528d442e8a7a7376e7255d6c9
adam-james <adam.j.sulewski@gmail.com>
2021-01-03 12:33:09 -0800

    add count for products added

--- lib/shop_web/live/page_live.ex
+++ lib/shop_web/live/page_live.ex
@@ -4,14 +4,12 @@ defmodule ShopWeb.PageLive do
   alias Shop.Catalog
   alias Shop.Shopper
 
-  # Add search
-
   @impl true
   def mount(params, %{"cart" => cart}, socket) do
     page = Catalog.list_products(params)
     if connected?(socket), do: ShopWeb.Endpoint.subscribe("cart:#{cart.id}")
 
-    {:ok, assign(socket, page: page, cart: cart)}
+    {:ok, assign(socket, page: page, cart: cart, products_added: 1)}
   end
 
   @impl true
@@ -25,8 +23,11 @@ defmodule ShopWeb.PageLive do
     product = Catalog.get_product!(id)
     {:ok, cart} = Shopper.add_to_cart(socket.assigns.cart, product)
     broadcast(cart)
-    socket = socket |> put_flash(:info, "Product added to cart.")
-    {:noreply, assign(socket, cart: cart)}
+
+    socket =
+      socket |> put_flash(:info, "Product added to cart (x #{socket.assigns.products_added}).")
+
+    {:noreply, assign(socket, cart: cart, products_added: socket.assigns.products_added + 1)}
   end
 
   @impl true
